9d63b275df7357ac3daad52dcc7aa6c1c00c6c88
isGavri <zssykes1@gmail.com>
2025-07-17 13:45:51 -0600

    Scanner
    The scanner is up and working

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Crafting compilers java interpreter by Robert Nymstrong
 
+### Running
+
 Run by typing
 
 `mvn compile`
@@ -9,4 +11,23 @@ Run by typing
 
 Run your own file by typing
 
+*You need to modify the 39 line on the pom.xml, deleting the arguments or setting you file name*
+
+#### Command
+
 `mvn -q exec:java -Dexec.args="filename"`
+
+#### pom.xml
+
+```
+    <arguments>
+        <argument>code.jlox</argument>
+    </arguments>
+```
+
+### Scanner/Lexer
+
+This is the first phase of our interpreter
+
+We start the interpreter by scanning the source (file or REPL) we implement a regular gramma (type 3) or finite automata to validate the input of the language, this only on a lexical level, this means only validates lexemes ("words"). We also assing the lexemes to tokens (so it has a higher abstraction) and handle errors that could occur on this phase (unterminated strings, unexpected characters), basically when the grammar fails.
+
--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -1,7 +1,9 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Scanner
@@ -17,10 +19,37 @@ public class Scanner {
   private int current = 0;
   private int line = 1;
 
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for", FOR);
+    keywords.put("fun", FUN);
+    keywords.put("if", IF);
+    keywords.put("nil", NIL);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super", SUPER);
+    keywords.put("this", THIS);
+    keywords.put("true", TRUE);
+    keywords.put("var", VAR);
+    keywords.put("while", WHILE);
+  }
+
   Scanner(String source) {
     this.source = source;
   }
 
+  /**
+   * Starts the scanning loop
+   * 
+   * @return
+   */
   List<Token> scanTokens() {
     while (!isAtEnd()) {
       start = current;
@@ -31,6 +60,9 @@ public class Scanner {
     return tokens;
   }
 
+  /**
+   * Scans tokens based of a characters from the source
+   */
   private void scanToken() {
     char c = advance();
     switch (c) {
@@ -38,7 +70,7 @@ public class Scanner {
         addToken(LEFT_PAREN);
         break;
       case ')':
-        addToken(RIGHT_BRACE);
+        addToken(RIGHT_PAREN);
         break;
       case '{':
         addToken(LEFT_BRACE);
@@ -78,26 +110,164 @@ public class Scanner {
         break;
       case '/':
         if (match('/')) {
-          // TODO: add support to c style block-comments
-          // Example: /* */
-          // NOTE: Must support nested comments, and keep in track lines
           while (peek() != '\n' && !isAtEnd())
             advance();
+        } else if (match('*')) {
+          scanBlockComment();
         } else {
           addToken(SLASH);
         }
+        break;
+      case ' ':
+      case '\r':
+      case '\t':
+        break;
+      case '\n':
+        line++;
+        break;
+      case '"':
+        string();
+        break;
       default:
-        Lox.error(line, "Unexpected character.");
+        if (isDigit(c)) {
+          number();
+        } else if (isAlpha(c)) {
+          identifier();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
         break;
     }
+
   }
 
+  /**
+   * Helper to know if a character is part of the alphabeth or _
+   * 
+   * @param c Character
+   * @return
+   */
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+  }
+
+  /**
+   * Helper to know if a characterr is alphanumeric
+   * 
+   * @param c Character
+   * @return
+   */
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+  /**
+   * Cosumes, scans and ads token for identifiers and reserved keywords
+   */
+  private void identifier() {
+    while (isAlphaNumeric(peek()))
+      advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null)
+      type = IDENTIFIER;
+    addToken(type);
+
+  }
+
+  /**
+   * Consumes, scans and ads token for numeric literals
+   */
+  private void number() {
+    while (isDigit(peek()))
+      advance();
+
+    if (peek() == '.' && isDigit(peekNext())) {
+      advance();
+
+      while (isDigit(peek()))
+        advance();
+    }
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  /**
+   * Peeks into the next character
+   * 
+   * @return
+   */
+  private char peekNext() {
+    if (current + 1 >= source.length())
+      return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
+  /**
+   * Helper to scan, consume and add token of string literals
+   */
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n')
+        line++;
+      advance();
+    }
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    advance();
+
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
+  }
+
+  /**
+   * Helper to scan a block comment, taking into account new lines and nested
+   * block comments
+   */
+  private void scanBlockComment() {
+    int depth = 1;
+
+    while (!isAtEnd()) {
+      char c = advance();
+
+      if (c == '\n') {
+        line++;
+      } else if (c == '/' && match('*')) {
+        depth++;
+      } else if (c == '*' && match('/')) {
+        depth--;
+        if (depth == 0)
+          return;
+      }
+    }
+
+    Lox.error(line, "Unterminated block comment.");
+  }
+
+  /**
+   * Peeks into the current character without consuming it
+   * 
+   * @return next character
+   */
   private char peek() {
     if (isAtEnd())
       return '\0';
     return source.charAt(current);
   }
 
+  /**
+   * Helper to consume next character if it matches an expected one
+   * 
+   * @param expected
+   * @return Whether it matched or not
+   */
   private boolean match(char expected) {
     if (isAtEnd()) {
       return false;
@@ -109,19 +279,42 @@ public class Scanner {
     return true;
   }
 
+  /**
+   * Consumes characters from the source
+   * 
+   * @return character at next position
+   */
   private char advance() {
     return source.charAt(current++);
   }
 
+  /**
+   * Add token to the list of tokens of the current scanned source without a
+   * literal value
+   * 
+   * @param type Token type
+   */
   private void addToken(TokenType type) {
     addToken(type, null);
   }
 
+  /**
+   * Add token to the list of tokens of the current scanned source
+   * 
+   * @param type    Token type
+   * @param literal Explicit value
+   */
   private void addToken(TokenType type, Object literal) {
     String text = source.substring(start, current);
     tokens.add(new Token(type, text, literal, line));
   }
 
+  /**
+   * Helper to check if the scanner is at the end of the source
+   * 
+   * @return true/false if the current scanned character is greater or equal than
+   *         the length of the source
+   */
   private boolean isAtEnd() {
     return current >= source.length();
   }
